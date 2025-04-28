57596377114aff3194f460f09d723f4fd35fdab7
DatkaiymDevNet <datkaiymtologonova27@gmail.com>
2025-04-28 02:40:07 +0000

    completed part 4

--- graphhopper_parse-json_3.py
+++ graphhopper_parse-json_3.py
@@ -12,9 +12,9 @@ loc2 = "Baltimore, Maryland"
 
 key = os.getenv("GRAPHHOPPER_API_KEY")
 
-print("GraphHopper API Key: " + key)
-
 def geocoding (location, key):
+    while location == "":
+        location = input("Enter the location again: ")
     geocode_url = "https://graphhopper.com/api/1/geocode?"
     url = geocode_url + urllib.parse.urlencode({"q":location, "limit": "1", "key":key})
 
@@ -22,7 +22,7 @@ def geocoding (location, key):
     json_data = replydata.json()
     json_status = replydata.status_code
 
-    if json_status == 200:
+    if json_status == 200 and len(json_data["hits"]) !=0:
         json_data = requests.get(url).json()
         lat = json_data["hits"][0]["point"]["lat"]
         lng = json_data["hits"][0]["point"]["lng"]
@@ -48,9 +48,21 @@ def geocoding (location, key):
         lat="null"
         lng="null"
         new_loc = location
+
+        # print(f'json_status {json_status}')
+        if json_status != 200:
+            print("Geocode API status: " + str(json_status) + "\nError message: " + json_data["message"])
     return json_status,lat,lng,new_loc
 
-orig = geocoding(loc1, key)
-print(orig)
-dest = geocoding(loc2, key)
-print(dest)
+while True:
+    loc1 = input("Starting Location: ")
+    if loc1 == "quit" or loc1 == "q":
+        break
+    orig = geocoding(loc1, key)
+    print(orig)
+    loc2 = input("Destination: ")
+    if loc2 == "quit" or loc2 == "q":
+        break
+    dest = geocoding(loc2, key)
+    print(dest)
+    
